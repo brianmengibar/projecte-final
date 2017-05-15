776575b663086ca03344bc16b3e06f0b018815ee
brian <brian@localhost.localdomain>
2017-05-15 20:39:08 +0200

    Update script install systemd-manager

--- scripts/install-systemd-manager.sh
+++ scripts/install-systemd-manager.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 # Nom: Brian Mengibar Garcia
 # Curs: hisx2
-# Sinopsys: Script per instal·lar "systemd-manager"
-#--------------------------------------------------
+# Sinopsys: Script per instal·lar l'eina "systemd-manager"
+#---------------------------------------------------------
 
 # Ens baixem un paquet exclusiu per Fedora 24
 sudo wget https://copr.fedorainfracloud.org/coprs/nunodias/systemd-manager/repo/fedora-24/nunodias-systemd-manager-fedora-24.repo -O /etc/yum.repos.d/nunodias-systemd-manager-fedora-24.repo
