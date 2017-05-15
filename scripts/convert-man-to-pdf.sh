836465861be73faa0528ad86c3d5f7c6222fdf02
brian mengibar <brianmengibargarcia@gmail.com>
2017-05-15 12:15:48 +0200

    Updates Script Convert To man To PDF

--- scripts/convert-man-to-pdf.sh
+++ scripts/convert-man-to-pdf.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Nom: Brian Mengibar Garcia
 # Curs: hisx2
-# Sinopsys: Script que crea un PDF de la pàgina manual desitjada
+# Synopsys: Script que crea un PDF de la pàgina manual desitjada
 #----------------------------------------------------------------
 man -t $1 | ps2pdf - "$1.pdf"
 exit 0
